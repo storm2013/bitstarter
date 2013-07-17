86c3c4f396121e4cb2c7eb40195f2aa24a266465
John Smith <example@stanford.edu>
2013-07-17 07:01:55 +0000

    update web.js - listen to port 8080

--- web.js
+++ web.js
@@ -1,21 +1,15 @@
 var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
-var fs = require ('fs');
-
-var data = fs.readFileSync('index.html');
-
-var buffer = new Buffer(data,'utf-8');
-
-var msg = buffer.toString('utf-8');
-
 app.get('/', function(request, response) {
-  response.send(msg);
-    //response.send("Hello World!");
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
